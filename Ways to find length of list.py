6f976bd9dd7c50d121b1e10b4058e1c50e92b1dc
pranav anand <42691222+pranavanand24@users.noreply.github.com>
2019-12-24 21:49:32 +0530

    Add files via upload

---
+++ Ways to find length of list.py
@@ -0,0 +1,2 @@
+n = len([10,20,30])
+print("The length of list is: ", n)
---
+++ program to find sum of elements in list.py
@@ -0,0 +1,7 @@
+total = 0
+list1 = [11, 5, 17, 18, 23]
+
+for ele in range(0, len(list1)):
+    total = total + list1[ele]
+
+print("Sum of all element in given list: ", total)
